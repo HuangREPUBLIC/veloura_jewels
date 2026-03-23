5778e286bc5e55495bffa6468b175a6048bdfd1d
jialinwu <jwuu0179@student.monash.edu>
2026-03-23 21:16:55 +1100

    Create replies view for contact submissions

---
+++ templates/ContactSubmissions/replies.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\ContactSubmission $contactSubmission
+ */
+?>
+<div class="contactSubmissions view content">
+    <h3><?= __('Replies for Contact Submission #{0}', $contactSubmission->id) ?></h3>
+
+    <div class="related">
+        <?php if (!empty($contactSubmission->contact_replies)): ?>
+            <table>
+                <thead>
+                <tr>
+                    <th><?= __('ID') ?></th>
+                    <th><?= __('Subject') ?></th>
+                    <th><?= __('Message') ?></th>
+                    <th><?= __('Sent At') ?></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($contactSubmission->contact_replies as $reply): ?>
+                    <tr>
+                        <td><?= h($reply->id) ?></td>
+                        <td><?= h($reply->subject) ?></td>
+                        <td><?= h($reply->message) ?></td>
+                        <td><?= h($reply->sent_at) ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else: ?>
+            <p><?= __('No replies have been sent yet.') ?></p>
+        <?php endif; ?>
+    </div>
+</div>
