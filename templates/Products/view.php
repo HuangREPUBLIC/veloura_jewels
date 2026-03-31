d3095d9bd18d380b5ba9098d00c654624f09bdfc
jialinwu <jwuu0179@student.monash.edu>
2026-03-31 19:12:58 +1100

    Redesigned product view page to match others view page

--- templates/Products/view.php
+++ templates/Products/view.php
@@ -3,106 +3,91 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Product $product
  */
+$this->assign('title', 'View Product');
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Product'), ['action' => 'edit', $product->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Product'), ['action' => 'delete', $product->id], ['confirm' => __('Are you sure you want to delete # {0}?', $product->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Products'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Product'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+<?php $this->Html->css('admincontact', ['block' => true]); ?>
+
+<div class="submissions-wrapper">
+    <div class="products view content">
+        <h3><?= h($product->name) ?></h3>
+
+        <div class="action-buttons">
+            <?= $this->Html->link(__('← Back to Products'), ['action' => 'index']) ?>
+
+            <?php
+            $identity = $this->request->getAttribute('identity');
+            $currentRole = $identity ? $identity->get('role') : null;
+            ?>
+
+            <?php if (in_array($currentRole, ['admin', 'part_time', 'full_time'])): ?>
+                <?= $this->Html->link(__('Edit'), ['action' => 'edit', $product->id]) ?>
+            <?php endif; ?>
+
+            <?php if (in_array($currentRole, ['admin', 'part_time', 'full_time'])): ?>
+                <?= $this->Form->postLink(
+                    __('Delete'),
+                    ['action' => 'delete', $product->id],
+                    [
+                        'confirm' => __('Are you sure you want to delete # {0}?', $product->id),
+                        'class' => 'btn-delete'
+                    ]
+                ) ?>
+            <?php endif; ?>
         </div>
-    </aside>
-    <div class="column column-80">
-        <div class="products view content">
-            <h3><?= h($product->name) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Name') ?></th>
-                    <td><?= h($product->name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Supplier Email') ?></th>
-                    <td><?= h($product->supplier_email) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($product->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Purchase Price') ?></th>
-                    <td><?= $this->Number->format($product->purchase_price) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Sale Price') ?></th>
-                    <td><?= $this->Number->format($product->sale_price) ?></td>
-                </tr>
-            </table>
-            <div class="related">
-                <h4><?= __('Related Categories') ?></h4>
-                <?php if (!empty($product->categories)) : ?>
-                <div class="table-responsive">
-                    <table>
-                        <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('Name') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
-                        </tr>
-                        <?php foreach ($product->categories as $category) : ?>
-                        <tr>
-                            <td><?= h($category->id) ?></td>
-                            <td><?= h($category->name) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'Categories', 'action' => 'view', $category->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'Categories', 'action' => 'edit', $category->id]) ?>
-                                <?= $this->Form->postLink(
-                                    __('Delete'),
-                                    ['controller' => 'Categories', 'action' => 'delete', $category->id],
-                                    [
-                                        'method' => 'delete',
-                                        'confirm' => __('Are you sure you want to delete # {0}?', $category->id),
-                                    ]
-                                ) ?>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </table>
-                </div>
-                <?php endif; ?>
-            </div>
-            <div class="related">
-                <h4><?= __('Related Product Images') ?></h4>
-                <?php if (!empty($product->product_images)) : ?>
-                <div class="table-responsive">
-                    <table>
-                        <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('Filename') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
-                        </tr>
-                        <?php foreach ($product->product_images as $productImage) : ?>
-                        <tr>
-                            <td><?= h($productImage->id) ?></td>
-                            <td><?= h($productImage->filename) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'ProductImages', 'action' => 'view', $productImage->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'ProductImages', 'action' => 'edit', $productImage->id]) ?>
-                                <?= $this->Form->postLink(
-                                    __('Delete'),
-                                    ['controller' => 'ProductImages', 'action' => 'delete', $productImage->id],
-                                    [
-                                        'method' => 'delete',
-                                        'confirm' => __('Are you sure you want to delete # {0}?', $productImage->id),
-                                    ]
-                                ) ?>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </table>
-                </div>
-                <?php endif; ?>
-            </div>
-        </div>
+
+        <table>
+            <tr>
+                <th><?= __('Id') ?></th>
+                <td><?= $this->Number->format($product->id) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Name') ?></th>
+                <td><?= h($product->name) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Supplier Email') ?></th>
+                <td><?= h($product->supplier_email) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Purchase Price') ?></th>
+                <td><?= $this->Number->format($product->purchase_price) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Sale Price') ?></th>
+                <td><?= $this->Number->format($product->sale_price) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Stock') ?></th>
+                <td>
+                    <?php if ($product->stock < 5): ?>
+                        <span style="color:red;font-weight:600;">
+                            <?= $product->stock ?>
+                        </span>
+                    <?php else: ?>
+                        <?= $product->stock ?>
+                    <?php endif; ?>
+                </td>
+            </tr>
+            <tr>
+                <th><?= __('Created') ?></th>
+                <td><?= h($product->created) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Modified') ?></th>
+                <td><?= h($product->modified) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Categories') ?></th>
+                <td>
+                    <?php
+                    if (!empty($product->categories)) {
+                        echo implode(', ', collection($product->categories)->extract('name')->toList());
+                    } else {
+                        echo '-';
+                    }
+                    ?>
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
