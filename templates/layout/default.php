45ee6aca560be03d78f192ecd99aac1de03c7041
jialinwu <jwuu0179@student.monash.edu>
2026-04-13 13:27:02 +1000

    add ‘add attachment' button on live chat

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -139,6 +139,9 @@ $_showChat = (
         </div>
 
         <div class="chat-popup-input-area">
+
+            </button>
+            <input type="file" id="chatFileInput" style="display:none" onchange="handleFileAttach(this)">
             <input
                 type="text"
                 id="chatInput"
@@ -153,8 +156,12 @@ $_showChat = (
                           d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"/>
                 </svg>
             </button>
+            <button class="chat-attach-btn" onclick="document.getElementById('chatFileInput').click()" title="Attach file">
+                <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" width="18" height="18">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"/>
+                </svg>
         </div>
-    </div>
 
     <script>
         var _chatHistory = [];
@@ -218,6 +225,13 @@ $_showChat = (
                     document.querySelector('.chat-send-btn').disabled = false;
                     input.focus();
                 });
+            function handleFileAttach(input) {
+                if (!input.files || !input.files[0]) return;
+                var file = input.files[0];
+                appendBubble('📎 ' + file.name, 'user');
+
+                input.value = '';
+            }
         }
     </script>
 <?php endif; ?>
