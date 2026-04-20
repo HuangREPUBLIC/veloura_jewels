24a2b77512a0e2d9bb2e3ee01dfe40380a0f5a66
jialinwu <jwuu0179@student.monash.edu>
2026-04-20 16:28:16 +1000

    Add branded 404 error page using default layout

--- templates/Error/error400.php
+++ templates/Error/error400.php
@@ -6,21 +6,27 @@
  */
 use Cake\Core\Configure;
 
-$this->setLayout('error');
+$this->setLayout('default');
 
 if (Configure::read('debug')) :
     $this->setLayout('dev_error');
-
     $this->assign('title', $message);
     $this->assign('templateName', 'error400.php');
-
     $this->start('file');
     echo $this->element('auto_table_warning');
     $this->end();
 endif;
+
+$this->assign('title', 'Page Not Found');
 ?>
-<h2><?= h($message) ?></h2>
-<p class="error">
-    <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= __d('cake', 'The requested address {0} was not found on this server.', "<strong>'{$url}'</strong>") ?>
-</p>
+
+<div class="error-page">
+    <p class="error-page__code">404</p>
+    <p class="error-page__title">Page Not Found</p>
+    <div class="error-page__divider"></div>
+    <p class="error-page__message">
+        We could not find the page you were looking for.<br>
+        Please use the navigation or the button below to go back to our website.
+    </p>
+    <?= $this->Html->link('Back to Homepage', '/', ['class' => 'error-page__btn']) ?>
+</div>
