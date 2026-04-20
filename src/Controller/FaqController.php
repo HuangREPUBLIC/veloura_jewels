037f6d6f77c94b38844924553afb20c047c5127a
Senuji Gamage <stha0082@student.monash.edu>
2026-04-21 00:28:15 +1000

    Create FAQ controller with static data

---
+++ src/Controller/FaqController.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+class FaqController extends AppController
+{
+    public function index()
+    {
+        $faqs = [
+            [
+                'question' => 'What products does Veloura Jewels sell?',
+                'answer' => 'Veloura Jewels offers handcrafted jewellery and home décor pieces.'
+            ],
+            [
+                'question' => 'How can I contact Veloura Jewels?',
+                'answer' => 'You can contact us via the Contact page.'
+            ],
+            [
+                'question' => 'Are your products handmade?',
+                'answer' => 'Yes, all our products are handcrafted.'
+            ]
+        ];
+
+        $this->set(compact('faqs'));
+    }
+}
